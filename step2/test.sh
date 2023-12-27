8570e0889545f66ccb413cb6a40b8955fc0943b4
JerryOctopus <140944134+JerryOctopus@users.noreply.github.com>
2023-12-28 00:06:11 +0800

    Delete step2 directory

--- step2/test.sh
+++
@@ -1,4 +0,0 @@
-#chmod +x test.sh
-CUDA_VISIBLE_DEVICES=2 python -m torch.distributed.launch --nproc_per_node=1 --master_port=4318 --use_env basicsr/test.py -opt ./options/test/S-C_dark_1_d100_wall.yml --launcher pytorch
-
-
